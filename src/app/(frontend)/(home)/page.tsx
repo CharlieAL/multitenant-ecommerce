43ff01a2cb9d1cecd0568030fe6726c958efd8a0
CharlieAl <charlie.lerma.ramirez@gmail.com>
2025-04-18 19:11:17 -0700

    feat: integrate tRPC for backend-frontend communication
    Add tRPC setup including routers, procedures, and client-side integration to enable type-safe API communication between the backend and frontend. This includes the creation of a categories router, query client, and TRPCReactProvider for seamless data fetching and state management.

--- src/app/(frontend)/(home)/page.tsx
+++ src/app/(frontend)/(home)/page.tsx
@@ -1,15 +1,25 @@
-import { Metadata } from 'next'
+// its a just testing file for trpc
 
-export const metadata: Metadata = {
-  title: 'Home | Funroad',
-  description: 'Welcome to Funroad - your multitenant ecommerce solution'
-}
+'use client'
+
+import { useQuery } from '@tanstack/react-query'
+import { useTRPC } from '~/trpc/client'
 
 const HomePage = () => {
+  const trpc = useTRPC()
+
+  const categories = useQuery(trpc.categories.getMany.queryOptions())
+
+  console.log(categories.data)
   return (
     <main className='container mx-auto py-8'>
       <h1 className='text-3xl font-bold mb-4'>Welcome to Funroad</h1>
-      <p>Hello world! Your ecommerce journey starts here.</p>
+      <p>
+        Hello world! Your ecommerce journey starts here. is Loading?: --{' '}
+        {categories.isLoading}
+      </p>
+      {categories.isLoading}
+      {JSON.stringify(categories.data)}
     </main>
   )
 }
