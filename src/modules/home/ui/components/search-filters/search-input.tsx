ca5acc14fa09de47bbeea07180d0d75cee488e8e
CharlieAl <charlie.lerma.ramirez@gmail.com>
2025-04-19 19:58:02 -0700

    feat(search-filters): enhance search filters with categories sidebar and type improvements
    - Add a new `CategoriesSidebar` component for mobile and desktop views
    - Update `SearchInput` to include a filter button for mobile
    - Refactor category types to use `CustomCategory` for better type safety
    - Improve category dropdown behavior and styling
    - Add a seed script for initial category data population

--- src/modules/home/ui/components/search-filters/search-input.tsx
+++ src/modules/home/ui/components/search-filters/search-input.tsx
@@ -1,17 +1,36 @@
-import { SearchIcon } from 'lucide-react'
+'use client'
+import { ListFilterIcon, SearchIcon } from 'lucide-react'
 import { Input } from '~/components/ui/input'
+import { CustomCategory } from '~/modules/categories/types'
+import { CategoriesSidebar } from './categories-sidebar'
+import { useState } from 'react'
+import { Button } from '~/components/ui/button'
 
 interface SearchInputProps {
   disabled?: boolean
+  data: CustomCategory[]
 }
 
-export const SearchInput = ({ disabled }: SearchInputProps) => {
+export const SearchInput = ({ disabled, data }: SearchInputProps) => {
+  const [isOpen, setIsOpen] = useState(false)
   return (
     <div className='flex items-center gap-2 w-full'>
+      <CategoriesSidebar data={data} isOpen={isOpen} onOpenChange={setIsOpen} />
       <div className='relative w-full'>
         <SearchIcon className='absolute top-1/2 left-3 -translate-y-1/2 size-4 text-neutral-500 ' />
-        <Input className='pl-8' placeholder='Search products' disabled={disabled} />
+        <Input
+          className='pl-8'
+          placeholder='Search products'
+          disabled={disabled}
+        />
       </div>
+      <Button
+        variant={'elevated'}
+        className='size-12 shrink-0 flex lg:hidden'
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <ListFilterIcon />
+      </Button>
     </div>
   )
 }
